be508bf0dc2c93673d6513f0af10771b3b28b5ca
GrygrFlzr <git@cybeast.dev>
2025-12-26 15:06:37 +0700

    chore: make runtime dependencies optional

--- packages/bot/tsdown.config.ts
+++ packages/bot/tsdown.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   // Don't bundle these - they need to be actual dependencies
-  external: ['postgres'],
+  external: ['bufferutil', 'postgres', 'utf-8-validate', 'zlib-sync'],
   // Bundle everything else including @gws/core
   noExternal: ['@gws/core', 'drizzle-orm', 'discord.js', 'bullmq', 'ioredis', 'opossum']
 });
